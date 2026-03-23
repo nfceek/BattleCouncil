a3fc73ed1d5726f1fea9d37af7f4cec204bd5d09
nfceek <bsmith@nfceek.com>
2026-03-23 16:54:10 -0400

    eod 2.04

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="<?= BASE_URL ?>/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
-  <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚔️</text></svg>">
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns='https://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚔️</text></svg>">
 </head>
 
 <body>
